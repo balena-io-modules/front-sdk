61ee569c60ceb4fd2c2971254e05209055072017
Kakhaber <kaxabernet@gmail.com>
2020-09-01 11:14:34 +0400

    Fix

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,6 @@ gulp.task('testRun', gulp.series('testBuild', () => {
 	if (process.env.JUNIT) {
 		reporter = 'mocha-junit-reporter';
 	}
-	console.log('reporter', reporter);
 	return gulp.src('./test/build/test/index.js', { read: false })
 		.pipe(mocha({ reporter: reporter, timeout: 10000 }))
 }));
