e6d3f7d4b00966e782fb0c04f907a6d170511251
Lucian Buzzo <lucian.buzzo@gmail.com>
2022-07-21 17:34:36 +0100

    Stop using gulp for building
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -7,49 +7,42 @@ const mocha = require('gulp-mocha');
 function getProject(test) {
 	const tsProject = typescript.createProject('tsconfig.json');
 	if (test) {
-		tsProject.config.include.push('./test/**/*.ts')
+		tsProject.config.include.push('./test/**/*.ts');
 	}
 
 	return tsProject;
 }
 
-// Compile the TS sources
-gulp.task('libBuild', () => {
-	const tsProject = getProject();
-	return tsProject.src()
-		.pipe(sourcemaps.init())
-		.pipe(tsProject())
-		.pipe(sourcemaps.write('./', { includeContent: true,
-			sourceRoot: '../lib'
-		}))
-		.pipe(gulp.dest('./build'))
-});
-
 gulp.task('testBuild', () => {
 	const tsProject = getProject(true);
-	tsProject.config.include.push('./test/*.ts')
-	return tsProject.src()
-		.pipe(tsProject())
-		.pipe(gulp.dest('./test/build'))
+	tsProject.config.include.push('./test/*.ts');
+	return tsProject.src().pipe(tsProject()).pipe(gulp.dest('./test/build'));
 });
 
-gulp.task('testRun', gulp.series('testBuild', () => {
-	let reporter = 'list';
-	if (process.env.JUNIT) {
-		reporter = 'mocha-junit-reporter';
-	}
-	return gulp.src('./test/build/test/index.js', { read: false })
-		.pipe(mocha({ reporter: reporter, timeout: 20000 }))
-}));
+gulp.task(
+	'testRun',
+	gulp.series('testBuild', () => {
+		let reporter = 'list';
+		if (process.env.JUNIT) {
+			reporter = 'mocha-junit-reporter';
+		}
+		return gulp
+			.src('./test/build/test/index.js', { read: false })
+			.pipe(mocha({ reporter: reporter, timeout: 20000 }));
+	}),
+);
 
 gulp.task('tslint', () => {
 	const tsProject = getProject(true);
-	return tsProject.src()
-		.pipe(tslint({
-			configuration: 'tslint.json',
-			formatter: 'prose'
-		}))
-		.pipe(tslint.report())
+	return tsProject
+		.src()
+		.pipe(
+			tslint({
+				configuration: 'tslint.json',
+				formatter: 'prose',
+			}),
+		)
+		.pipe(tslint.report());
 });
 
 gulp.task('build', gulp.series('libBuild'));
